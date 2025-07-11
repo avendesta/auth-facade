7631b76b4ef93bf0c42ca55c8d61e7921d6a07a4
avendesta <53615807+avendesta@users.noreply.github.com>
2025-07-11 15:36:45 -0500

    remove unnecessary file created for testing purpose

--- src/main/java/com/apple/auth_facade/controller/AuthController.java
+++ src/main/java/com/apple/auth_facade/controller/AuthController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.apple.auth_facade.dto.ExternalResponse;
 
 import reactor.core.publisher.Mono;
 
